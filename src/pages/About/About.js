dc8676831e6dd26208e66c8301a513b8adb669ce
Geshna <ga95@duke.edu>
2020-05-31 15:49:08 -0400

    added text to about page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Title from '../../components/Title/Title';
 import './About.css';
 import backgroundImage from '../../images/about-background-image.png';
 
@@ -10,6 +9,12 @@ export default class About extends Component {
                 <img src= {backgroundImage} alt="backgroundImage" className="backgroundImage"/>
 
                 <div className = "backgroundBox">
+                    <p className = "title">EARTHY CREATIONS</p>
+                    <p className = "mission">Our Mission is to help people see that 
+                    the things they use once don’t just disappear once they are thrown in the trash. 
+                    Rather, they remain and can be reused or even re-invented as beautiful objects. 
+                    We help aspiring artists, primarily college students, support their dream by 
+                    reaching a broader audience.</p>
                     
                 </div>
                 
